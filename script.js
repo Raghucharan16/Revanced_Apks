6f116b203d9c62aac4d6bdaed1c34f43fa000018
GitHub Action <action@github.com>
2024-10-16 18:51:56 +0000

    Update APKs for version v

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-window.onload = function() {
-    // The array containing the APK filenames is defined in the GitHub Action
-    var apkContainer = document.getElementById('apk-list');
-
-    // Check if apkList exists
-    if (typeof apkList === 'undefined') {
-        console.error('apkList is undefined!');
-        apkContainer.innerHTML = '<p>Error: No APKs available.</p>';
-        return;
-    }
-
-    if (apkList.length === 0) {
-        var message = document.createElement('p');
-        message.textContent = 'No APKs available for download at the moment.';
-        apkContainer.appendChild(message);
-    } else {
-        apkList.forEach(function(apk) {
-            var link = document.createElement('a');
-            link.href = './releases/' + apk;
-            link.textContent = apk;
-            link.download = apk;
-            apkContainer.appendChild(link);
-            apkContainer.appendChild(document.createElement('br')); // Add line break
-        });
-    }
-};
+var apkList = [];
+apkList.push('music-revanced-extended-v7.16.53-arm64-v8a.apk');
+apkList.push('youtube-revanced-extended-v19.16.39-all.apk');
+apkList.push('youtube-revanced-v19.16.39-all.apk');
