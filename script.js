7554e9d5d9722dbf5580c5e4f284946b74344cc3
Narra_Venkata_Raghu_Charan <narravenkataraghucharan@gmail.com>
2024-10-16 20:05:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const apkList = document.getElementById('apk-list');
-
-    // List of APK files (will be automatically updated by the workflow)
-    const apks = [
-        '',
-        ''
-    ];
-
-    // Dynamically create download links
-    apks.forEach(apk => {
-        const apkLink = document.createElement('a');
-        apkLink.href = `releases/${apk}`;
-        apkLink.className = 'apk-link';
-        apkLink.textContent = `Download ${apk}`;
-        apkList.appendChild(apkLink);
+window.onload = function() {
+    var apkContainer = document.getElementById('apk-list');
+    apkList.forEach(function(apk) {
+        var link = document.createElement('a');
+        link.href = './releases/' + apk;
+        link.textContent = apk;
+        apkContainer.appendChild(link);
+        apkContainer.appendChild(document.createElement('br')); // Add line break
     });
-});
+};
